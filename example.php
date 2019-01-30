123e319cfc0ee4ccfd834f8ad3b203ff204c6969
Martins <olekss.martins@gmail.com>
2019-01-30 14:54:40 +0200

    Update HTML output, add other WIP stuff

--- example.php
+++ example.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $config = parse_ini_file('config.ini');
 $inputFilename = $config['input_filename'];
 $outputFilename = $config['output_filename'];
@@ -12,6 +12,10 @@ $revisedData = array();
 $output = '';
 $defaultKey = 'unknown';
 
+// URL that will be replaced befor HTML output
+// TODO: Replace with config from INI
+define('BASE_URL','/var/www/project/');
+
 function writeOut($content, $file)
 {
 	$outputFile = fopen($file, 'w');
@@ -22,47 +26,62 @@ function writeOut($content, $file)
 
 // Get summary for all Code Sniff
 // print_r($jsonData->totals);
-foreach($jsonData->files as $file => $data) {
-	//echo 'FILE: ' . $file . PHP_EOL;
-	//echo '- ERRORS: ' . $data->errors . PHP_EOL;
-	//echo '- WARNINGS: ' . $data->warnings . PHP_EOL;
-	foreach($data->messages as $item) {
-		// Restructure data - group by sniff ID
-                if (isset($item->source)) {
-			$key = $item->source;
-                } else {
-			$key = $defaultkey;
-                }		
-		$revisedData[$key][] = array(
-			'ref_path' => $file . ':' . $item->line, // full_path:line
-			'ref_filename' => basename($file), // only filename
-			'line' => $item->line, // only code line
-			'message' => $item->message
-		);
+foreach ($jsonData->files as $file => $data) {
+    //echo 'FILE: ' . $file . PHP_EOL;
+    //echo '- ERRORS: ' . $data->errors . PHP_EOL;
+    //echo '- WARNINGS: ' . $data->warnings . PHP_EOL;
+    foreach ($data->messages as $item) {
+        // Restructure data - group by ID
+        if (isset($item->source)) {
+            $key = $item->source;
+        } else {
+            $key = $defaultKey;
+		}
+		//TODO: Add more fields from sniffs
+        $revisedData[$key][] = array(
+            'ref_path' => $file . ':' . $item->line, // full_path:line
+            'ref_filename' => basename($file), // only filename
+            'line' => $item->line, // only code line
+            'message' => $item->message
+        );
 
-		// Just outputting info
-		//echo '-- MESSAGE:' . $item->message . PHP_EOL;
-		//echo '-- TYPE:' . $item->type . PHP_EOL; 
-		//echo '-- LINE:' . $item->line . PHP_EOL;
-		//echo PHP_EOL;
-		// format file:line
-		//echo $file . ':' . $item->line . PHP_EOL;
-		//echo "\t" . $item->message . PHP_EOL;
-	}
+        // Just outputting info
+        //echo '-- MESSAGE:' . $item->message . PHP_EOL;
+        //echo '-- TYPE:' . $item->type . PHP_EOL;
+        //echo '-- LINE:' . $item->line . PHP_EOL;
+        //echo PHP_EOL;
+        // format file:line
+        //echo $file . ':' . $item->line . PHP_EOL;
+        //echo "\t" . $item->message . PHP_EOL;
+    }
 }
+?>
 
-
-// Test revised data
-foreach($revisedData as $sniff => $data) {
-	echo $sniff . PHP_EOL;
-	$output .= $sniff . PHP_EOL;
-	foreach($data as $file) {
-		echo "\t" . $file['ref_path'] . PHP_EOL;
-		$output .= $file['ref_path'] . PHP_EOL;
-		//echo "\t\t" . $file['ref_filename'] . PHP_EOL;
-		//echo "\t\t" . $file['message'] . PHP_EOL;
-	}
-	echo PHP_EOL;
-}
-// Write results to file
-writeOut($output, $outputFilename);
+<!-- OUTPUT -->
+<html>
+	<head>
+		<meta charset="UTF-8">
+	</head> 
+	<body>
+	<style>
+	* { font-size: 12px; } 
+	table { border-collapse: collapse; } 
+	table td { border: 1px solid #f3f3f3; } 
+	.sniff-name, .sniff-count { padding-top: 10px; background-color: #f0f0f0; font-weight: bold; }
+	</style>
+	<table>
+	<?php foreach ($revisedData as $sniff => $data): ?>    
+		<tr>
+			<td class="sniff-name"><?= $sniff ?></td>
+			<td class="sniff-count">Count:<?= count($revisedData[$sniff]) ?></td>
+		</tr>    
+		<?php foreach ($data as $file): ?>
+		<tr>
+			<td class="path"><?= str_replace(BASE_URL, '', $file['ref_path']) ?></td>
+			<td class="message"><?= $file['message'] ?></td>
+		</tr>    
+		<?php endforeach; ?>    
+	<?php endforeach; ?>
+	</table>
+	</body>
+</html>
